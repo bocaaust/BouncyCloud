ae7d0de55633a89d4020f8dc6c91a68323afc638
Austin Lubetkin <bocaaust@gmail.com>
2016-10-03 21:18:55 -0400

    Updated Implementation

---
+++ Updated Implementation-PreLaunch/BouncyCloud/BouncyCloud/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Bouncy Cloud
+//
+//  Created by Austin Lubetkin on Monday, October 3, 2016
+//  Copyright (c) Austin Lubetkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class StandaloneCodeaViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) StandaloneCodeaViewController *viewController;
+
+@end
